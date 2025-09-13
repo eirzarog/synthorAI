a4a44792732f14139e428be7298a3d1a34665b65
eirzarog <eirzarog@gmail.com>
2025-09-14 02:35:39 +0300

    Public UserDTO and custom serCriteria as query parameters in business logic

--- synthor-api/src/main/java/dev/eirzarog/synthor/api/services/MessageService.java
+++ synthor-api/src/main/java/dev/eirzarog/synthor/api/services/MessageService.java
@@ -1,4 +1,9 @@
 package dev.eirzarog.synthor.api.services;
 
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class MessageService {
 }
--- synthor-api/src/main/java/dev/eirzarog/synthor/api/services/ModelService.java
+++ synthor-api/src/main/java/dev/eirzarog/synthor/api/services/ModelService.java
@@ -1,4 +1,10 @@
 package dev.eirzarog.synthor.api.services;
 
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class ModelService {
 }
